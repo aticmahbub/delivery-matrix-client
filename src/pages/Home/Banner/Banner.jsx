64571d948c65b19b8cb2bd36f11c18b2aca14cf7
aticcmahbub <aticcmahbub@gmail.com>
2024-08-31 09:20:52 +0600

    banner edited

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -17,8 +17,8 @@ const Banner = () => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-neutral-content text-center">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Welcome</h1>
-                    <p className="mb-5">
+                    <h1 className="mb-5 text-5xl text-new-green font-bold">Welcome</h1>
+                    <p className="mb-5 text-secondary-green">
                     Discover the Best Deals on Delivery Matrix! <br />Shop Now for Exclusive Discounts and Unbeatable Prices
                     </p>
                     <Button className="">Shop Now</Button>
